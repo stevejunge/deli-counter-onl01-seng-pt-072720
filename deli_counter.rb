a147745ebd6b0202c1520e62fa7b8f7fe9f9559e
Stephen Junge <steve.junge@gmail.com>
2020-08-05 03:26:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,43 @@
-def line(people)
-  katz_deli []
-  if katz_deli = []
-    return "The line is currently empty."
+katz_deli = []
+#line
+def line(katz_deli)
+  phrase = "The line is currently: "
+  if katz_deli.length > 0 
+    katz_deli.each_with_index do |name, index|
+      phrase += "#{index + 1}. #{name} "
+    end
+    puts phrase
+  else
+    puts "The line is currently empty."
   end
-  
-  
+end
+
+#take a number
 def take_a_number(katz_deli, name)
-  
+	katz_deli.push(name)
+	puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+#	end
+end
+
+
+#now serving
+def now_serving(katz_deli)
+  if katz_deli == 0
+    puts "There is nobody waiting to be served!"
+  else 
+	 puts "Currently serving " + katz_deli[0] +"."
+  end
+  katz_deli.shift
+end
+
+
+take_a_number(katz_deli, "Igor")
+take_a_number(katz_deli, "Bob")
+take_a_number(katz_deli, "Melanie")
+ line(katz_deli)
+ now_serving(katz_deli)
+ line(katz_deli)
+ take_a_number(katz_deli, "Corgi")
+ line(katz_deli)
+ now_serving(katz_deli)
+ line(katz_deli)
